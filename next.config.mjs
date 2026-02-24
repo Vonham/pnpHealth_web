f9b1edaf89db2ddb99992a1df5c360a39af8a80f
SungminCho <cho@delvine.co.kr>
2026-02-24 16:14:47 +0900

    Restore original project configurations and exclude edit_ref from git

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/pnpHealth_web', // GitHub Pages 레포지토리 이름
   typescript: {
     ignoreBuildErrors: true,
   },
